f6ac7cc1b0b08dc0b6e8ac89aa886b6d89e81db5
nickbespalov <nbespalov@icloud.com>
2016-07-11 00:21:50 +0500

    rxjs powered xhr util

--- utils/XHR.js
+++ utils/XHR.js
@@ -1,44 +1,52 @@
-/**
- * Utils module
- * @module utils/XHR
- */
-
 const getPath = require('./objects').getPath;
-
-
+const extend = require('extend');
+const Observable = require('rxjs/rx').Observable;
 /**
- * @description Get XHR data
- * @param {Object} options - The options
+ * @function onXHRLoaded
+ * @throws SyntaxError - Throws SyntaxError if response is not a JSON string
+ * @param res
+ * @param rej
+ * @param e
+ */
+function onXHRLoaded(res, rej, e) {
+    const  parsedResponse = JSON.parse(getPath(e, 'target.responseText'));
+    getPath(e, 'target.status') === 200 ? res(parsedResponse) : rej(parsedResponse);
+}
+/**
+ *
+ * @param {XMLHttpRequest} xhr - The xhr object
+ * @param {String} k - The key value
+ * @param {Object} collection - The headers object
+ */
+function setXHRHeader (xhr, collection, k) {
+    xhr.setRequestHeader(k, collection[k]);
+}
+/**
+ * * @param options
  * @param  {String} options.method - The method name (GET, POST, PUT)
  * @param  {String} options.url - The url to make request
  * @param  {Object} options.data - The request payload data
  * @param  {Object} options.headers - The request headers. By default is set to {'Content-Type':'application/json; charset=UTF-8'}
  * @param  {Boolean} options.async - Specify is this request is asynchronous or a synchronous
- * @returns {Promise}
+ * @returns {Observable<T>}
  */
-module.exports.xhr = function XHR (options) {
-    return new Promise(function xhrPromiseExecutor(res, rej) {
+function getJSON (options) {
+    const xhr$ = Observable.fromPromise(new Promise(function xhrPromiseExecutor(res, rej) {
         const xhr = new XMLHttpRequest();
-        const headers = getPath(options, 'headers') || {'Content-Type': 'application/json; charset=UTF-8'};
+        const headers = extend(true, {}, getPath(options, 'headers'), {'Content-Type': 'application/json; charset=UTF-8'});
         const data = getPath(options, 'data') ? JSON.stringify(getPath(options, 'data')) : null;
         const async = typeof getPath(options, 'async') === 'undefined' ? true : getPath(options, 'async');
-        xhr.onload = function onXHRLoaded(e) {
-            let parsedResponse;
-            try {
-                parsedResponse = JSON.parse(getPath(e, 'target.responseText'));
-            } catch (err) {
-                parsedResponse = new Error('Bad server response');
-            }
-            if (getPath(e, 'target.status') === 200) {
-                res(parsedResponse);
-            }
-            else {
-                rej(parsedResponse);
-            }
-        };
+        debugger
+        xhr.onload = onXHRLoaded.bind(xhr, res, rej);
         xhr.open(getPath(options, 'method'), getPath(options, 'url'), async);
-        headers && Object.keys(headers).forEach(k=> xhr.setRequestHeader(k, getPath(headers, k)));
+        headers && Object.keys(headers).forEach(setXHRHeader.bind(null, xhr, headers));
         xhr.send(data);
-    });
+    }));
+    return xhr$;
+
+}
+
+module.exports = {
+    getJSON: getJSON
 };
 
